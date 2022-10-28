83dba63e646dd209dd8ec3c20af9e3c8b52c3aba
roshankhaire <roshankhaire1992@gmail.com>
2022-10-28 19:46:55 +0530

    Amount and Title Update

--- myapp/src/components/Expenses/ExpenseItem.js
+++ myapp/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,20 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
+import React,{useState} from 'react'
 
 function ExpenseItem(props){
+    const [title,setTitle]=useState(props.title)
+    const [amount,setAmount]=useState(props.amount)
     function DeleteExpense(){
         console.log("delete item")
+    }
+    function ExpenseAmountUpdate(){
+
+      setAmount(100)
+    }
+    function ExpenseTitleUpdate(){
+          setTitle("updated")
     }
     
     return(
@@ -12,10 +22,15 @@ function ExpenseItem(props){
         <ExpenseDate date={props.date}/>
         
         <div className="expense-item__discription">
-            <h2>{props.title}</h2>
+            <h2>{title}
+             <button onClick={ExpenseTitleUpdate}>Update Title</button>
+            </h2>
             </div>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}
+        <button onClick={ExpenseAmountUpdate}>ExpenseAmountUpdate</button>
+        </div>
         <button onClick={DeleteExpense}>Delete Expense</button>
+       
         </Card>
     )
 }

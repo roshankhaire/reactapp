e1f02f98474f93e206baf05a050e8f6a14a23ff8
roshankhaire <roshankhaire1992@gmail.com>
2022-10-29 16:20:05 +0530

    submitform

--- myapp/src/components/ExpenseForm/ExpenseForm.js
+++ myapp/src/components/ExpenseForm/ExpenseForm.js
@@ -1,30 +1,67 @@
 import React,{useState}from "react";
 import './ExpenseForm.css';
 const ExpenseForm=()=>{
-    const [enteredTitle,setenteredTitle]=useState('');
-    const [enteredAmount,setenteredAmount]=useState('');
-    const [enteredDate,setenteredDate]=useState('');
+     const [enteredTitle,setenteredTitle]=useState('');
+     const [enteredAmount,setenteredAmount]=useState('');
+     const [enteredDate,setenteredDate]=useState('');
+    // const [userInput,setUserInput]=useState({
+    //      enteredTitle:'',
+    //      enteredAmount:'',
+    //      enteredDate:''
+
+    // })
+
+
+    
     const titleChangeHandler=(event)=>{
-        setenteredTitle(event.target.value)
+        
+            setenteredTitle(event.target.value)
+    
+        
+       
     }
     const AmountChangeHandler=(event)=>{
         setenteredAmount(event.target.value)
+        
     }
     const DateChangeHandler=(event)=>{
         setenteredDate(event.target.value)
+        
     }
+    const submitHandler=(event)=>{
+        event.preventDefault();
+        const expenseData={
+            title:enteredTitle,
+            amount:enteredAmount,
+           
+            date:new Date(enteredDate)
+        }
+        console.log(expenseData)
+    }
+    
     return (
-        <div>
-            <form>
-               <label> Expense title</label>
-               <input type="text" onChange={titleChangeHandler}/>
+        
+            <form onSubmit={submitHandler}>
+                <div>
+                <label> Expense title</label>
+                <input type="text" onChange={titleChangeHandler}/>
+                </div>
+                <div>
+                      
                <label>Expense Amount</label>
-               <input type="Amount" onClick={AmountChangeHandler}/>
-               <label> Date</label>
-               <input type="text" onClick={DateChangeHandler}/>
-               <button>submit</button>
+               <input type="number" onClick={AmountChangeHandler} min="0.01" step="0.01"/>
+                </div>
+                 <div>
+                 <label> Date</label>
+               <input type="date" onClick={DateChangeHandler}/>
+                 </div>
+               
+              <div>
+              <button onClick={submitHandler}>submit</button>
+              </div>
+              
             </form>
-        </div>
+        
     )
 
 }

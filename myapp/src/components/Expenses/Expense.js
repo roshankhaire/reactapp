edaf8e9bae731689b0fdcbd241c94295e1981481
roshankhaire <roshankhaire1992@gmail.com>
2022-10-31 18:33:16 +0530

    conditional render

--- myapp/src/components/Expenses/Expense.js
+++ myapp/src/components/Expenses/Expense.js
@@ -10,18 +10,23 @@ import React, { useState } from 'react';
         const filterChangeHandeler=selectedYear=>{
             setFilteredYear(selectedYear)
         };
+       const filteredExpense=props.item.filter(expense=>{
+        return expense.date.getFullYear().toString()===filteredYear
+       })
     
     return(
         <div>
             <Card className="expenses">
              <ExpenseFilter  selected={filteredYear} onChangeFilter={filterChangeHandeler}/>
-              {props.item.map(expense=>
+             {filteredExpense.length===0 && <p>No Expense Found.</p>}
+             {filteredExpense.length>0 && (filteredExpense.map(expense=>
                              <ExpenseItem
                                key={expense.id}
                                 title={expense.title}
                                amount={expense.amount} 
-                                date={expense.date}/>)}
-              
+                                date={expense.date}/>))}
+            
+                            
           
                
                 
